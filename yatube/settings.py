64cafcededc9dc02a083d0da639ea70bc0d04dc7
Kirill Antonov <antonovkirill@yandex.ru>
2020-04-15 16:57:48 +0300

    STATICFILES_STORAGE add for testing

--- yatube/settings.py
+++ yatube/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
