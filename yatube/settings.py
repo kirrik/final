90a2c0c0a4eaab4a8666c02d596318a4b554baf7
Kirill Antonov <antonovkirill@yandex.ru>
2020-04-15 17:12:59 +0300

    DEBUG to True

--- yatube/settings.py
+++ yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b*0q1w-lsh+peag$_)g88ttcqnq91$)i7=&lta1^2@8=bjyh-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "for-cto-school.herokuapp.com",
