6bc012eb05248ec8813679560458cdf094b737bb
Kirill Antonov <antonovkirill@yandex.ru>
2020-04-15 13:03:34 +0300

    trying to disable collectstatic

--- settings.py
+++ settings.py
@@ -20,6 +20,7 @@ django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DISABLE_COLLECTSTATIC=1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
